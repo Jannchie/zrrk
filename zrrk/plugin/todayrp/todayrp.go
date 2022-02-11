73133ea61d0fe09834848912535f98a8b755061c
Jannchie <jannchie@gmail.com>
2022-02-11 20:20:08 +0900

    fix: todayrp

--- zrrk/plugin/todayrp/todayrp.go
+++ zrrk/plugin/todayrp/todayrp.go
@@ -3,6 +3,7 @@ package todayrp
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 
 	"gorm.io/driver/sqlite"
@@ -27,21 +28,27 @@ func New() *TodayRPPlugin {
 }
 
 func (p *TodayRPPlugin) HandleDanmuData(data zrrk.DanmakuData) string {
+	if !strings.Contains(data.Text, "06") {
+		return ""	
+	}
+	if !(strings.Contains(data.Text, "RP") || strings.Contains(data.Text, "rp") || strings.Contains(data.Text, "人品")) {
+		return ""
+	}
 	if data.User.UID == 0 {
 		return ""
 	}
 	var rp TodayRP
 	_ = DB.Find(&rp, "uid = ?", data.User.UID)
 	yp, mp, dp := time.Unix(rp.CreatedAt.Unix(), 0).Date()
-	y, m, d := time.Unix(time.Now().UTC().UnixMilli(), 0).Date()
+	y, m, d := time.Unix(time.Now().Unix(), 0).Date()
 	if yp == y && m == mp && d == dp {
-		msg := fmt.Sprintf("%s今天的RP为%d", data.User.Name, rp.RP)
+		msg := fmt.Sprintf("%s今天已经测过，今天的RP为%d。", data.User.Name, rp.RP)
 		return msg
 	}
 	rp.UID = data.User.UID
 	rp.RP = int(rand.NormFloat64()*50 + 50)
 	DB.Save(&rp)
-	msg := fmt.Sprintf("%s今天的RP是%d", data.User.Name, rp.RP)
+	msg := fmt.Sprintf("%s今天的RP是%d。", data.User.Name, rp.RP)
 	return msg
 }
 
