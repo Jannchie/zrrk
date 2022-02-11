14f6e2b8a821607b2a296211f1d063364dba8fbf
Jannchie <jannchie@gmail.com>
2022-02-11 19:29:56 +0900

    feat: plugin

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
-	"jannchie.com/zeroroku/zrrk"
+	"jannchie.com/zrrk/zrrk"
+	"jannchie.com/zrrk/zrrk/plugin/todayrp"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags)
 	bot := zrrk.Default(545068)
+	bot.AddPlugin(todayrp.New())
 	bot.Connect()
 	done := make(chan struct{})
 	<-done
